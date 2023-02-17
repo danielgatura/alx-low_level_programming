a282fe6cd27ba264567c09170f9811bdabcabeab
danielgatura <gaturadan@gmail.com>
2023-02-17 15:02:30 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -program to assign a radom number to variable n
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if ((n % 10) > 5)
+	{
+	printf("last digit of %d is %d and is grater than 5", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	}
+	printf("last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+	}
+	else
+	{
+	printf("last digit of %d is %d and is 0", n, n % 10);
+	}
+	return (0);
+}
