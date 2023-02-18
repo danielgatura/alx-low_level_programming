3b77188736c9935c70850b93f93475a251a1463d
danielgatura <gaturadan@gmail.com>
2023-02-17 17:16:25 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the number in base 16
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
