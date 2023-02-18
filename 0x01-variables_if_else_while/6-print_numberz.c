473ff0fe0b4978f52fd648182ad4e3d0e87155d6
danielgatura <gaturadan@gmail.com>
2023-02-17 16:51:14 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program to print single digit
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
