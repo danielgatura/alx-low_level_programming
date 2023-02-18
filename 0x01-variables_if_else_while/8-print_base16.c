3a3e3ed94c05069747e71421f33c8341195549b9
danielgatura <gaturadan@gmail.com>
2023-02-17 17:19:00 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (lc = 'a'; lc <= 'z'; lc++)
+	for (lc = 'a'; lc <= 'f'; lc++)
 		putchar(lc);
 
 	putchar('\n');
