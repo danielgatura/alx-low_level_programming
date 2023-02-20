5380b8a4dcab9105b4e2f8665b77efede859bf88
danielgatura <gaturadan@gmail.com>
2023-02-20 13:40:36 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int c, d;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (d = c + 1; d <= 9; d++)
+		for (d = c + 1; d <= '9'; d++)
 		{
 		if (c != d)
 		{
 		putchar(c);
 		putchar(d);
-			if (c == 8 && d == 9)
+			if (c == '8' && d == '9')
 			{
 			continue;
 			putchar(',');
