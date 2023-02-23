ea0cffcf551b858f6c235052173967403f02c5cc
danielgatura <gaturadan@gmail.com>
2023-02-23 14:25:08 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal -function to print diagonal line
+ * @n: the number of time line will be drawn
+ * Return: empty
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (x <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	for (y = 0; y < x; y++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+}
