b335f360258ed6745d77edf90ac146fd388363c7
danielgatura <gaturadan@gmail.com>
2023-02-23 14:57:00 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function print square
+ * @size: how big the square will be
+ * Return:empyt
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (x <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	 _putchar('\n');
+	}
+	}
+}
