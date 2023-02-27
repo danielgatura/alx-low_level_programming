a6bec069814a1095c6e336fdc27b0413b306c4eb
danielgatura <gaturadan@gmail.com>
2023-02-27 13:11:44 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,8 @@
  *Return: int
  */
 int _atoi(char *s)
-{	int c = 0;
+{	
+	int c = 0;
 	unsigned int ni = 0;
 	int min = 1;
 	int isi = 0;
@@ -20,7 +21,7 @@ int _atoi(char *s)
 	while (s[c] >= 48 && s[c] <= 57)
 	{
 	isi = 1;
-	ni = (ni * 10) + (s[c] + '0');
+	ni = (ni * 10) + (s[c] - '0');
 	c++;
 	}
 	if (isi == 1)
